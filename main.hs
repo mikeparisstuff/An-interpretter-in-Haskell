b689be487cfbe735c6107ca76d4a8d5bea5e2043
Michael Paris <mlparis92@gmail.com>
2014-04-09 13:06:12 -0400

    expr parsing for math, constants, comparison

--- main.hs
+++ main.hs
@@ -7,7 +7,21 @@ type LineNo = Int
 
 data Class = Class String [Attribute] deriving(Show)
 data Attribute = Attribute Name Type (Maybe Expr) deriving(Show)
-data Expr = Integer LineNo Type Int deriving (Show)
+data Expr = Integer LineNo Type Int
+        |   Str LineNo Type String
+        |   TrueBool LineNo Type
+        |   FalseBool LineNo Type
+        |   Equal LineNo Type Expr Expr
+        |   LessThan LineNo Type Expr Expr
+        |   LessEqual LineNo Type Expr Expr
+        |   Negate LineNo Type Expr
+        |   IsVoid LineNo Type Expr
+        |   Not LineNo Type Expr
+        |   Times LineNo Type Expr Expr
+        |   Divide LineNo Type Expr Expr
+        |   Plus LineNo Type Expr Expr
+        |   Minus LineNo Type Expr Expr
+    deriving (Show)
 
 
 -- Read in the class map from .cl-type and create data structure
@@ -27,18 +41,79 @@ parse_cm_attribute _ [] = error "empty attribute in class map"
 parse_cm_attribute 0 rem_input = ([], rem_input)
 parse_cm_attribute n xs = case xs of 
     ("initializer" : attr_name : attr_type : tl) -> 
-        let (expr, rem_input) = parse_cm_expr tl
+        let (expr, rem_input) = parse_expr tl
             (attrs, fin_rem_input) = parse_cm_attribute (n-1) rem_input
         in ((Attribute attr_name attr_type (Just expr) ) : attrs, fin_rem_input)
     ("no_initializer" : attr_name : attr_type : tl) -> 
         let (attrs, rem_input) = parse_cm_attribute (n-1) tl
         in ((Attribute attr_name attr_type Nothing) : attrs, rem_input)
 
-parse_cm_expr xs = case xs of
+parse_expr xs = case xs of
+    -- Integer, String, and Bool
     (line_no : expr_type : "integer" : val : tl) ->
         let n = read line_no :: Int
             v = read val :: Int 
         in ((Integer n expr_type v), tl)
+    (line_no : expr_type : "string" : val : tl) ->
+        let n = read line_no :: Int
+        in ((Str n expr_type val), tl)
+    (line_no : expr_type : "true" : tl) ->
+        let n = read line_no :: Int
+        in ((TrueBool n expr_type), tl)
+    (line_no : expr_type : "false" : tl) ->
+        let n = read line_no :: Int
+        in ((FalseBool n expr_type), tl)
+    -- LT, LE, EQ
+    (line_no : expr_type : "eq" : tl) ->
+        let n = read line_no :: Int
+            (expr1, rem_input_1) = parse_expr tl
+            (expr2, rem_input_2) = parse_expr rem_input_1
+        in ((Equal n expr_type expr1 expr2), rem_input_2)
+    (line_no : expr_type : "lt" : tl) ->
+        let n = read line_no :: Int
+            (expr1, rem_input_1) = parse_expr tl
+            (expr2, rem_input_2) = parse_expr rem_input_1
+        in ((LessThan n expr_type expr1 expr2), rem_input_2)
+    (line_no : expr_type : "le" : tl) ->
+        let n = read line_no :: Int
+            (expr1, rem_input_1) = parse_expr tl
+            (expr2, rem_input_2) = parse_expr rem_input_1
+        in ((LessEqual n expr_type expr1 expr2), rem_input_2)
+    -- Not, Negate, IsVoid
+    (line_no : expr_type : "negate" : tl) ->
+        let n = read line_no :: Int
+            (expr, rem_input) = parse_expr tl
+        in ((Negate n expr_type expr), rem_input)
+    (line_no : expr_type : "not" : tl) ->
+        let n = read line_no :: Int
+            (expr, rem_input) = parse_expr tl
+        in ((Not n expr_type expr), rem_input)
+    (line_no : expr_type : "isvoid" : tl) ->
+        let n = read line_no :: Int
+            (expr, rem_input) = parse_expr tl
+        in ((IsVoid n expr_type expr), rem_input)
+    -- Plus, Minus, Times, Divide
+    (line_no : expr_type : "times" : tl) ->
+        let n = read line_no :: Int
+            (expr1, rem_input_1) = parse_expr tl
+            (expr2, rem_input_2) = parse_expr rem_input_1
+        in ((Times n expr_type expr1 expr2), rem_input_2)
+    (line_no : expr_type : "divide" : tl) ->
+        let n = read line_no :: Int
+            (expr1, rem_input_1) = parse_expr tl
+            (expr2, rem_input_2) = parse_expr rem_input_1
+        in ((Divide n expr_type expr1 expr2), rem_input_2)
+    (line_no : expr_type : "plus" : tl) ->
+        let n = read line_no :: Int
+            (expr1, rem_input_1) = parse_expr tl
+            (expr2, rem_input_2) = parse_expr rem_input_1
+        in ((Plus n expr_type expr1 expr2), rem_input_2)
+    (line_no : expr_type : "minus" : tl) ->
+        let n = read line_no :: Int
+            (expr1, rem_input_1) = parse_expr tl
+            (expr2, rem_input_2) = parse_expr rem_input_1
+        in ((Minus n expr_type expr1 expr2), rem_input_2)
+    
     _ -> error "could not match class map expr"
 
 -- Main Execution
--- simple.cl
+++ simple.cl
@@ -1,6 +1,17 @@
 class Main inherits IO {
-    x : Int <- 5;
-    y : Int;
+    x : String <- "hello";
+    y : String;
+    a : Int <- ~4;
+    b : Int <- 5 * 2;
+    c : Bool <- true;
+    d : Bool <- not false;
+    e : Object <- 7 = 8;
+    f : Object <- 2 < 3;
+    g : Object <- 4 <= 5;
+    h : Int <- 3 + 2;
+    i : Int <- 8 - 2;
+    j : Int <- 4 / 2;
+    k : Bool <- isvoid(5);
     main() : Object {
         abort()
     };
